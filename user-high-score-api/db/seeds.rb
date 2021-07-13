585cb70669e637789ab99269e3e0df24466d3948
russell-macalintal <russell-macalintal@gmail.com>
2021-07-13 00:28:03 -0400

    Provide seed data to test fetch() functionality

--- user-high-score-api/db/seeds.rb
+++ user-high-score-api/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'securerandom'
+
+# Populate database with random users
+users = ['Ethan', 'Sarah', 'Stephanie', 'Peter', 'Eric', 'Brian', 'Andy', 'Marissa', 'Steve', 'David']
+users.each do |user|
+    User.create(username: user)
+end
+
+# Add 3 difficulty levels
+Difficulty.create(level: "Easy")
+Difficulty.create(level: "Medium")
+Difficulty.create(level: "Hard")
+
+# Populate database with random scores for users and difficulty levels
+(1..50).each do |score|
+    u_id = (SecureRandom.random_number(users.size) + 1).floor
+    d_id = (SecureRandom.random_number(3) + 1).floor
+    s = (SecureRandom.random_number(1800) + 1).floor
+    Score.create(score: s, user_id: u_id, difficulty_id: d_id)
+end
