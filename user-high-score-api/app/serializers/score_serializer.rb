44f36c03bc4a845cb41c572a25ceb557846693cf
russell-macalintal <russell-macalintal@gmail.com>
2021-07-13 02:18:50 -0400

    Minor refactor of ScoreSerializer

--- user-high-score-api/app/serializers/score_serializer.rb
+++ user-high-score-api/app/serializers/score_serializer.rb
@@ -1,6 +1,6 @@
 class ScoreSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
-  belongs_to :user
-  belongs_to :difficulty
+  attributes :user, :difficulty
+  # belongs_to :user
+  # belongs_to :difficulty
 end
