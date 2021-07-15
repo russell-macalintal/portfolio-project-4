58c52c016f7ef3937991f94000b39a32838071ea
russell-macalintal <russell-macalintal@gmail.com>
2021-07-15 01:31:17 -0400

    Added menu bar to HTML and basic CSS styling

--- game-js/src/index.js
+++ game-js/src/index.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+const BASE_URL = "http://localhost:3000";
+const USERS_URL = `${BASE_URL}/users`;
+const SCORES_URL = `${BASE_URL}/scores`;
+const DIFFICULTIES_URL = `${BASE_URL}/difficulties`;
