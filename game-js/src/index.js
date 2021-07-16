9aa050b0e5e4f58da3d8658f0d6a10729d9520b7
russell-macalintal <russell-macalintal@gmail.com>
2021-07-16 01:23:17 -0400

    Modified CSS styling for menu links and added Game, User, and Score JS classes

--- game-js/src/index.js
+++ game-js/src/index.js
@@ -1,6 +1,12 @@
-console.log("Hello World");
-
 const BASE_URL = "http://localhost:3000";
 const USERS_URL = `${BASE_URL}/users`;
 const SCORES_URL = `${BASE_URL}/scores`;
 const DIFFICULTIES_URL = `${BASE_URL}/difficulties`;
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch(DIFFICULTIES_URL)
+        .then( response => response.json() )
+        .then( function( diff_obj) {
+            console.log( diff_obj );
+        })
+})
